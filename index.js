b8188ab4a16c1b6573d5d96273c15bd1e2721f13
Joseph Ruff <joegruff16@gmail.com>
2024-07-20 14:42:52 -0400

    Installed all dependencies. Set up index.js with starter code and it might also be completed. Commented out my Thought model to explain how Reaction will be a subdocument and not a Model. Updated my package.json file with correct info.

---
+++ index.js
@@ -0,0 +1,24 @@
+// Insert packagtes below that we will leverage for this app
+const express = require("express");
+require("dotenv").config();
+// Establishes a connection our database
+const db = require("./config/connection");
+// Creates a route path
+const routes = require("./routes");
+
+// Establishes which port to use and if others aren't available it will use 3001
+const PORT = process.env.PORT || 3001;
+// Here we are leveraging middleware
+const app = express();
+
+// Our middleware configuration functionality
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(routes);
+
+// This is a database event listener that will listen for connections on the specified port
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server is running on port ${PORT}`);
+  });
+});
---
+++ models/Thought.js
@@ -0,0 +1,3 @@
+// The Reaction schema isn't it's own model because it's a subdocument within this Thought model
+
+// Reaction Schema
