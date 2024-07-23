254378c10f0f6c41dc4e8b7803a6874dfedd11b4
Joseph Ruff <joegruff16@gmail.com>
2024-07-23 18:22:06 -0400

    Added express router dependency. Updated index.js files to establish routes.

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+// Install packages for routes
+const router = require("express").Router();
+
+// Create variables to store route
+const thoughtRoutes = require("./thoughtRoutes");
+const userRoutes = require("./userRoutes");
+
+router.use("/user", userRoutes);
+router.use("/thought", thoughtRoutes);
+
+module.exports = router;
