a42bee0e146db0841ac35ed931cf08015b46bdc8
Joseph Ruff <joegruff16@gmail.com>
2024-07-23 18:15:44 -0400

    Pseudo coded next steps to create all routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,18 @@
+// `/api/thoughts`
+
+// Get route to get all thoughts
+
+// Need a GET route to get a single thought by _id
+
+// POST route to creat e a new thought. Tip: don't forget to push the created thought's `_id` to the associated user's `thoughts` array field
+
+// `PUT` to update a thought by its `_id`
+
+// `DELETE` to remove a thought by its `_id`
+
+// May create a new route file if reactions makes it look too big
+// `/api/thoughts/:thoughtId/reactions`
+
+// `POST` to create a reaction stored in a single thought's `reactions` array field
+
+// `DELETE` to pull and remove a reaction by the reaction's `reactionId` value
---
+++ routes/api/userRouters.js
@@ -0,0 +1,18 @@
+// We need routes`/api/users:
+
+// to GET all users
+
+// Get a single user by _id
+
+// Post route to add a new user
+
+// Put route to update a user by their _id
+
+// Delete route to remove a user by _id
+
+// We could possibly make a new route all together for this I will see how clean it looks
+// `/api/users/:userId/friends/:friendId`
+
+// POST route to add a new friend to the user's friend list
+
+// Delete route to remove a friend from the friend list
