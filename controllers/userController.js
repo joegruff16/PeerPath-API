86b052fb807e096809604261f82f617362a31c80
Joseph Ruff <joegruff16@gmail.com>
2024-07-30 22:06:24 -0400

    Added a delete and put route to delete and update a user by id.

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,4 +53,17 @@ module.exports = {
     }
   },
   // Delete route to remove a user by _id
+  async deleteUser(req, res) {
+    try {
+      const deletedUser = await User.findOneAndDelete({
+        _id: req.params.userId,
+      });
+      if (!deletedUser) {
+        return res.status(404).json({ message: "No user found with this ID" });
+      }
+      res.status(200).json({ message: "User deleted" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
